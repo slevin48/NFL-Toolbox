39afe9b84fcb957439b39e926fe3fc806a1ada46
slevin48 <yann.debray@gadz.org>
2025-11-01 22:23:54 -0400

    remove .uv

--- .uv/env.fish
+++
@@ -1,4 +0,0 @@
-if not contains "$HOME/Documents/nfl-toolbox/.uv" $PATH
-    # Prepending path in case a system-installed binary needs to be overridden
-    set -x PATH "$HOME/Documents/nfl-toolbox/.uv" $PATH
-end
